e3ca9ead09179fdf4d4ee87e133857a82c0f9949
Ryan Miller <rmille70@uwyo.edu>
2024-04-27 11:51:56 -0600

    Finished depthFirstSearch()

--- code.js
+++ code.js
@@ -1,3 +1,22 @@
-function depthFirstSearch(graph, startNode, targetNode) {
-    return [];
+// Referenced: 
+// https://www.geeksforgeeks.org/depth-first-search-or-dfs-for-a-graph/
+// https://en.wikipedia.org/wiki/Depth-first_search
+
+function depthFirstSearch(adjMatrix, startNode, targetNode) {
+    if (!adjMatrix || !adjMatrix.length) { return []; }
+    let visited = []; 
+    function dfsFinder(currentNode, path) {
+        visited.push(currentNode);
+        if (currentNode == targetNode) { return path.concat(currentNode); }
+        for (let adj = 0; adj < adjMatrix[currentNode].length; adj++) {
+            if (!visited.includes(adj) && adjMatrix[currentNode][adj] == 1) {
+                const foundPath = dfsFinder(adj, path.concat(currentNode));
+                if (foundPath) { return foundPath; }
+            }
+        }
+        return [];
+    }
+    let sol = dfsFinder(startNode, []);
+    return sol;
 }
+
